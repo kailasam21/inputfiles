5b488be6bbb6f574fe47f81664418947e5b1aa91
kailasam21 <kailasamp2020@gmail.com>
2024-04-03 11:28:22 +0530

    sample commit

---
+++ Newfiles/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path
+from bheem import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name="home"),
+    path('uploadfile',views.uploadfile,name="uploadfile"),
+    path('deleteFile/<int:id>',views.deleteFile), 
+]
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ bheem/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BheemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bheem'
